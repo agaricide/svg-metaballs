f5ad7131c720123be62944c5f928aa7d495b4336
agaricide <agaricide@gmail.com>
2019-08-05 00:56:34 -0500

    First pass labeling and cleanup of metaball script

--- ts-svg-metaballs/src/metaball.ts
+++ ts-svg-metaballs/src/metaball.ts
@@ -1,83 +1,71 @@
 import { dist, getVector, angle } from "./utils";
 const HALF_PI = Math.PI / 2;
 
-function metaball(
-  radius1: number,
-  radius2: number,
+function makeMetaballGoo(
+  r1: number,
+  r2: number,
   center1: number[],
   center2: number[],
   handleSize = 2.4,
   v = 0.5
 ) {
   const d = dist(center1, center2);
-  const maxDist = radius1 + radius2 * 2.5;
+  const maxDist = r1 + r2 * 2.5;
   let u1 = 0;
   let u2 = 0;
 
-  if (radius1 === 0 || radius2 === 0) {
+  if (r1 <= 0 || r2 <= 0 || d > maxDist || d <= Math.abs(r1 - r2)) {
     return "";
   }
 
-  if (d > maxDist || d <= Math.abs(radius1 - radius2)) {
-    return "";
+  if (d < r1 + r2) {
+    u1 = Math.acos((r1 * r1 + d * d - r2 * r2) / (2 * r1 * d));
+    u2 = Math.acos((r2 * r2 + d * d - r1 * r1) / (2 * r2 * d));
   }
 
-  if (d < radius1 + radius2) {
-    u1 = Math.acos(
-      (radius1 * radius1 + d * d - radius2 * radius2) / (2 * radius1 * d)
-    );
-    u2 = Math.acos(
-      (radius2 * radius2 + d * d - radius1 * radius1) / (2 * radius2 * d)
-    );
-  }
-
-  // All the angles
   const angleBetweenCenters = angle(center2, center1);
-  const abc = angleBetweenCenters;
-  const maxSpread = Math.acos((radius1 - radius2) / d);
+  const maxSpread = Math.acos((r1 - r2) / d);
 
   const angle1 = angleBetweenCenters + u1 + (maxSpread - u1) * v;
   const angle2 = angleBetweenCenters - u1 - (maxSpread - u1) * v;
-  const angle3 = abc + Math.PI - u2 - (Math.PI - u2 - maxSpread) * v;
+  const angle3 =
+    angleBetweenCenters + Math.PI - u2 - (Math.PI - u2 - maxSpread) * v;
   const angle4 =
     angleBetweenCenters - Math.PI + u2 + (Math.PI - u2 - maxSpread) * v;
-  // Points
-  const p1 = getVector(center1, angle1, radius1);
-  const p2 = getVector(center1, angle2, radius1);
-  const p3 = getVector(center2, angle3, radius2);
-  const p4 = getVector(center2, angle4, radius2);
+
+  const p1 = getVector(center1, angle1, r1);
+  const p2 = getVector(center1, angle2, r1);
+  const p3 = getVector(center2, angle3, r2);
+  const p4 = getVector(center2, angle4, r2);
 
   // Define handle length by the
   // distance between both ends of the curve
-  const totalRadius = radius1 + radius2;
+  const totalRadius = r1 + r2;
   const d2Base = Math.min(v * handleSize, dist(p1, p3) / totalRadius);
 
   // Take into account when circles are overlapping
-  const d2 = d2Base * Math.min(1, (d * 2) / (radius1 + radius2));
+  const d2 = d2Base * Math.min(1, (d * 2) / (r1 + r2));
 
-  const r1 = radius1 * d2;
-  const r2 = radius2 * d2;
+  // Handle length
+  const hl1 = r1 * d2;
+  const hl2 = r2 * d2;
 
-  const h1 = getVector(p1, angle1 - HALF_PI, r1);
-  const h2 = getVector(p2, angle2 + HALF_PI, r1);
-  const h3 = getVector(p3, angle3 + HALF_PI, r2);
-  const h4 = getVector(p4, angle4 - HALF_PI, r2);
+  const h1 = getVector(p1, angle1 - HALF_PI, hl1);
+  const h2 = getVector(p2, angle2 + HALF_PI, hl1);
+  const h3 = getVector(p3, angle3 + HALF_PI, hl2);
+  const h4 = getVector(p4, angle4 - HALF_PI, hl2);
 
-  return metaballToPath(p1, p2, p3, p4, h1, h2, h3, h4, d > radius1, radius2);
+  return toGooPath([p1, p2, p3, p4], [h1, h2, h3, h4], d > r1, r2);
 }
 
-function metaballToPath(
-  p1: any,
-  p2: any,
-  p3: any,
-  p4: any,
-  h1: any,
-  h2: any,
-  h3: any,
-  h4: any,
+function toGooPath(
+  points: number[][],
+  handles: number[][],
   escaped: any,
   r: any
 ) {
+  const [p1, p2, p3, p4] = points;
+  const [h1, h2, h3, h4] = handles;
   return [
     "M",
     p1,
@@ -99,4 +87,4 @@ function metaballToPath(
   ].join(" ");
 }
 
-export default metaball;
+export default makeMetaballGoo;

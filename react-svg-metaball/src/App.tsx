aea939a33ba71eb6ad6cf587fadb3b7d628ef169
agaricide <agaricide@gmail.com>
2019-08-08 00:32:00 -0500

    Added react-spring for some animation

--- react-svg-metaball/src/App.tsx
+++ react-svg-metaball/src/App.tsx
@@ -2,12 +2,13 @@ import React, { useState, useRef } from "react";
 import { toSVGCoord } from "./metaball/utils/dom";
 import { Point } from "./metaball/types/Point";
 import makeGoo from "./metaball/metaball";
+import { Spring, config } from "react-spring/renderprops";
 
 import "./App.css";
 
 const CIRCLE_1: Point = [400, 600];
-const R1 = 96;
-const R2 = 64;
+const R1 = 80;
+const R2 = 100;
 
 const App: React.FC = () => {
   const [circleCoord, setCircleCoord] = useState([0, 0]);
@@ -29,11 +30,24 @@ const App: React.FC = () => {
       ref={svgEl}
       onMouseMove={handleMouseMove}
     >
-      <g ref={gEl} strokeWidth="6" fill="#fff" stroke="#333">
-        <circle id="circle1" cx={CIRCLE_1[0]} cy={CIRCLE_1[1]} r={R1} />
-        <circle id="circle2" cx={x} cy={y} r={R2} stroke="none" />
-        <path id="goo" d={makeGoo(R1, R2, CIRCLE_1, [x, y])} />
-      </g>
+      <Spring
+        config={config.molasses}
+        from={{ coords: [400, 600] }}
+        to={{ coords: [x, y] }}
+      >
+        {(props: any) => (
+          <g ref={gEl} strokeWidth="6" fill="#fff" stroke="#333">
+            <circle
+              id="circle1"
+              cx={props.coords[0]}
+              cy={props.coords[1]}
+              r={R1}
+            />
+            <circle id="circle2" cx={x} cy={y} r={R2} stroke="none" />
+            <path id="goo" d={makeGoo(R1, R2, props.coords, [x, y])} />
+          </g>
+        )}
+      </Spring>
     </svg>
   );
 };

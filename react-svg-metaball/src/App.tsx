28e9b7f8cc4822b8e9266c3575b09d10a0372058
agaricide <agaricide@gmail.com>
2019-08-08 02:38:34 -0500

    Made goo draggable

--- react-svg-metaball/src/App.tsx
+++ react-svg-metaball/src/App.tsx
@@ -1,43 +1,54 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, Fragment } from "react";
 import { toSVGCoord } from "./metaball/utils/dom";
 import { Point } from "./metaball/types/Point";
 import makeGoo from "./metaball/metaball";
 import { Spring, config } from "react-spring/renderprops";
+import GithubBadge from "./github-badge";
 
 import "./App.css";
 
 const STARTING_POINT: Point = [600, 350];
-const R1 = 90;
-const R2 = 70;
+const R1 = 100;
+const R2 = 75;
 
 const App: React.FC = () => {
   const [mouseCoord, setMouseCoord] = useState(STARTING_POINT);
+  const [isMouseDown, setMouseDown] = useState(false);
   const gEl = useRef<SVGGElement>(null);
   const svgEl = useRef<SVGSVGElement>(null);
 
   const handleMouseMove = (event: React.MouseEvent) => {
-    if (!svgEl.current || !gEl.current) return;
+    if (!svgEl.current || !gEl.current || !isMouseDown) return;
     setMouseCoord(toSVGCoord(event, svgEl.current, gEl.current));
   };
 
-  const [x, y] = mouseCoord;
+  const grabbingClassName = isMouseDown ? "grabbing" : "";
 
   return (
-    <svg viewBox="0 0 1200 1200" ref={svgEl} onMouseMove={handleMouseMove}>
-      <Spring
-        config={config.molasses}
-        from={{ coord: STARTING_POINT }}
-        to={{ coord: [x, y] }}
+    <Fragment>
+      <GithubBadge className={grabbingClassName} />
+      <svg
+        ref={svgEl}
+        viewBox="0 0 1200 1200"
+        onMouseMove={handleMouseMove}
+        onMouseDown={() => setMouseDown(true)}
+        onMouseUp={() => setMouseDown(false)}
       >
-        {(animate: any) => (
-          <g ref={gEl}>
-            <circle cx={x} cy={y} r={R1} />
-            <circle cx={animate.coord[0]} cy={animate.coord[1]} r={R2} />
-            <path d={makeGoo(R1, R2, [x, y], animate.coord)} />
-          </g>
-        )}
-      </Spring>
-    </svg>
+        <Spring
+          config={config.molasses}
+          from={{ coord: STARTING_POINT }}
+          to={{ coord: mouseCoord }}
+        >
+          {animation => (
+            <g ref={gEl} className={grabbingClassName}>
+              <circle cx={mouseCoord[0]} cy={mouseCoord[1]} r={R1} />
+              <circle cx={animation.coord[0]} cy={animation.coord[1]} r={R2} />
+              <path d={makeGoo(R1, R2, mouseCoord, animation.coord)} />
+            </g>
+          )}
+        </Spring>
+      </svg>
+    </Fragment>
   );
 };
 
